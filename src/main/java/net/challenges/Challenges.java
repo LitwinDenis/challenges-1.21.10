82cf0d3208579f7f6baaf7ed45ebe00417b39f9b
denis <denislitwin@googlemail.com>
2026-01-12 14:18:37 +0100

    Initial commit

---
+++ src/main/java/net/challenges/Challenges.java
@@ -0,0 +1,18 @@
+package net.challenges;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Challenges implements ModInitializer {
+	public static final String MOD_ID = "challenges";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/challenges/challengesclient.java
@@ -0,0 +1,10 @@
+package net.challenges;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class challengesclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
